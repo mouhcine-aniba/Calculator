a53717e2ddc008e3aa6d59a94ed7280a42d4d46c
mouhc <mouhc@MOUHCINE>
2022-03-11 16:49:51 +0100

    Add project files.

---
+++ Calculator/Icalc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal interface Icalc
+    {
+        double addition(double a,double b);
+        double addition(List<double> vals);
+
+        double Substraction(double a, double b);
+        double Multiplication(double a, double b);
+        double Division(double a, double b);
+        double SquareRoot(double a, double b);
+
+    }
+}
